1d852f89251b9553940d2b4fe629a896f14346e8
JohanurRahman <johanur.rahman@gmail.com>
2018-12-14 22:40:07 +0600

    Basic Components Created and JWT installed

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { RequestResetComponent } from './components/password/request-reset/request-reset.component';
+import { ResponseResetComponent } from './components/password/response-reset/response-reset.component';
+import { SignupComponent } from './components/password/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    NavbarComponent,
+    LoginComponent,
+    RequestResetComponent,
+    ResponseResetComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule
